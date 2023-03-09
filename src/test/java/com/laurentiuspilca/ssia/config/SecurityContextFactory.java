9891bdcd8611bda3d8164189d1efc0c5d95dba2a
Thanin-Pintong <thaninp@live.com>
2023-03-09 23:56:43 +0700

    Review

--- src/test/java/com/laurentiuspilca/ssia/config/SecurityContextFactory.java
+++ src/test/java/com/laurentiuspilca/ssia/config/SecurityContextFactory.java
@@ -1,5 +1,6 @@
 package com.laurentiuspilca.ssia.config;
 
+import java.util.Arrays;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,7 +10,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SecurityContextFactory
@@ -19,12 +21,17 @@ public class SecurityContextFactory
     public SecurityContext createSecurityContext(MockCustomUser mockCustomUser) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
 
-        Collection<? extends GrantedAuthority > authorities = List.of(() -> "read");
+        Collection<? extends GrantedAuthority > authorities = //List.of(() -> "read");
+                Collections.unmodifiableList(Arrays.asList(() -> "read"));
         String clientId = "client_id";
 
-        OAuth2User principal = new DefaultOAuth2User(authorities, Map.of(clientId, clientId), clientId);
+        Map m = new HashMap(); // for Java 8
+        m.put(clientId, clientId); // for Java 8
+        OAuth2User principal = new DefaultOAuth2User(authorities, //Map.of(clientId, clientId), clientId);
+                m, clientId);
 
-        var a = new OAuth2AuthenticationToken(principal, authorities, clientId);
+        //var a = new OAuth2AuthenticationToken(principal, authorities, clientId);
+        OAuth2AuthenticationToken a = new OAuth2AuthenticationToken(principal, authorities, clientId);
         context.setAuthentication(a);
         return context;
     }
